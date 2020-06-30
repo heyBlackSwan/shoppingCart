696e119afd7871152cb2b48ca487097defb8da0b
blackSwan <tongyuan.liang@wealthbetter.com>
2020-06-30 18:41:16 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <h1>hello {{ title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "app",
+  data() {
+    return {
+      title: "test project"
+    };
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  color: red;
 }
 </style>
---
+++ src/components/cart.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>购物车页面</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cart",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+p
+  font-size 16px
+  color #ff1559
+</style>
